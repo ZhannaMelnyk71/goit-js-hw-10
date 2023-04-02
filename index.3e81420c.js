55adaa0fca961d073344b958ce6752fa2f5ee989
ZhannaMelnyk71 <112390937+ZhannaMelnyk71@users.noreply.github.com>
2023-04-02 21:48:04 +0000

    Deploying to gh-pages from @ ZhannaMelnyk71/goit-js-hw-10@975afda4a386b8a4fbf2fd7b3c2182ac124ad3ce 🚀

---
+++ index.3e81420c.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/deutschland").then((function(n){return n.json()})).then(console.log());
+//# sourceMappingURL=index.3e81420c.js.map
---
+++ index.679077f2.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/deutschland").then((e=>e.json())).then(console.log());
+//# sourceMappingURL=index.679077f2.js.map
